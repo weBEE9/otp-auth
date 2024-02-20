186b627fe55d70ff5b6ccc46cd94622232425ffd
weeee9 <teletubby332@gmail.com>
2024-02-20 19:29:06 +0800

    feat: improve mock implementation for TTL functionality
    - Add time package import to `mock/repository_mock.go` and `mock/service_mock.go`
    - Add TTL method to `mock/repository_mock.go` and `mock/service_mock.go`
    - Add TTL method recorder to `mock/repository_mock.go` and `mock/service_mock.go`
    
    Signed-off-by: weeee9 <teletubby332@gmail.com>

--- mock/service_mock.go
+++ mock/service_mock.go
@@ -12,6 +12,7 @@ package mock
 import (
 	context "context"
 	reflect "reflect"
+	time "time"
 
 	gomock "go.uber.org/mock/gomock"
 )
@@ -52,6 +53,21 @@ func (m *MockOTPService) GenOTP(arg0 context.Context, arg1 string) (string, erro
 func (mr *MockOTPServiceMockRecorder) GenOTP(arg0, arg1 any) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "GenOTP", reflect.TypeOf((*MockOTPService)(nil).GenOTP), arg0, arg1)
+}
+
+// TTL mocks base method.
+func (m *MockOTPService) TTL(arg0 context.Context, arg1 string) (time.Duration, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "TTL", arg0, arg1)
+	ret0, _ := ret[0].(time.Duration)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// TTL indicates an expected call of TTL.
+func (mr *MockOTPServiceMockRecorder) TTL(arg0, arg1 any) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "TTL", reflect.TypeOf((*MockOTPService)(nil).TTL), arg0, arg1)
 }
 
 // VerifyOTP mocks base method.
